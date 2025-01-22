294b823f76cba43c8b72c2ea97fc958a0d645738
onioktairawan <122547358+onioktairawan@users.noreply.github.com>
2025-01-22 17:47:15 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -36,19 +36,11 @@ def admin_required(func):
 
 # Fungsi untuk menyambut anggota baru
 async def welcome_new_member(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    welcome_image_url = "https://i.pinimg.com/236x/df/3d/30/df3d309603c24015cfe0f5cd06ad596f.jpg"
     welcome_text = "Selamat datang di grup, {}! 😊 Jangan lupa baca aturan grup dan jadikan tempat ini menyenangkan untuk semua!"
     
     for member in update.message.new_chat_members:
-        # Cek jika pengguna memiliki foto profil
-        if member.photo:
-            # Ambil foto profil pengguna
-            profile_picture = member.photo[-1].file_id
-            # Kirim foto profil pengguna sebagai gambar selamat datang
-            await update.message.reply_photo(profile_picture, caption=welcome_text.format(member.first_name))
-        else:
-            # Jika tidak ada foto profil, kirim gambar default
-            welcome_image_url = "https://i.pinimg.com/236x/df/3d/30/df3d309603c24015cfe0f5cd06ad596f.jpg"
-            await update.message.reply_photo(welcome_image_url, caption=welcome_text.format(member.first_name))
+        await update.message.reply_photo(welcome_image_url, caption=welcome_text.format(member.first_name))
 
 # Fungsi untuk menangani perintah /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
